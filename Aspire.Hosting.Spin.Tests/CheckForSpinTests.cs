82bf0487320744b256b45b95e4b13251628210e9
Thorsten Hans <thorsten.hans@fermyon.com>
2024-05-29 10:59:00 +0200

    tests: Run CheckForSpinTests conditionally
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- Aspire.Hosting.Spin.Tests/CheckForSpinTests.cs
+++ Aspire.Hosting.Spin.Tests/CheckForSpinTests.cs
@@ -4,13 +4,39 @@ namespace Aspire.Hosting.Spin.Tests;
 
 public class CheckForSpinTests
 {
-    [Fact]
+    
+    bool IsSpinInPath()
+    {
+        var paths = Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator);
+        foreach (var path in paths)
+        {
+            var fullPath = Path.Combine(path, "spin");
+            if (File.Exists(fullPath))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    [SkippableFact]
     public async Task ItShouldPassIfSpinIsInstalled()
     {
+        Skip.IfNot(IsSpinInPath());
         var sut = new CheckForSpin();
         var act = () => sut.BeforeStartAsync(null);
-
+        
         await act.Should().NotThrowAsync();
+    }
+    
+    [SkippableFact]
+    public async Task ItShouldFailIfSpinIsNotInstalled()
+    {
+        Skip.If(IsSpinInPath());
+        var sut = new CheckForSpin();
+        var act = () => sut.BeforeStartAsync(null);
+        
+        await act.Should().ThrowAsync<Exception>();
     }
 
 }
